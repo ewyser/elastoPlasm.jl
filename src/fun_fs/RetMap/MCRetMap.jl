35fc8c7c62dda23cc2a4c38ff00c35f229182ed1
ewyser <manuwyser@gmail.com>
2024-10-01 15:46:20 +0200

    minor fix

--- src/fun_fs/RetMap/MCRetMap.jl
+++ src/fun_fs/RetMap/MCRetMap.jl
@@ -4,11 +4,12 @@
     else
         w = zeros(mpD.nmp,mpD.nmp)
         for i ∈ 1:mpD.nmp
-            for j ∈ 1:mpD.nmp
+            for j ∈ i:mpD.nmp
                 ξ = (mpD.x[i,1]-mpD.x[j,1]) 
                 η = (mpD.x[i,2]-mpD.x[j,2])
                 d = sqrt(ξ^2+η^2)
                 w[i,j] = d/ls*exp(-(d/ls)^2)
+                w[j,i] = d/ls*exp(-(d/ls)^2)
             end
         end
         w    = w./sum(w,dims=2)
