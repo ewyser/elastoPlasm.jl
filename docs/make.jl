ac20da01271fc75c0d710b00534dbb95f6da73b1
ewyser <manuwyser@gmail.com>
2024-10-11 15:02:37 +0200

    resolve #5 + fix minor stuffs

--- docs/make.jl
+++ docs/make.jl
@@ -3,11 +3,57 @@ push!(LOAD_PATH,"../src/")
 include("./src/fun/utils.jl")
 using Documenter,elastoPlasm
 
+
+
+function mdGenerate(file="function.md",path="docs/src")
+    # concatenate in a single stirng the list all function declaration in api
+    libs = Dict()
+    for (k,lib) ∈ enumerate(keys(elastoPlasm.sys.lib))
+        lib_list = undef
+        for (l,val) ∈ enumerate(elastoPlasm.sys.lib[lib])
+            if l == 1
+                lib_list = "\"$(val)\","
+            else
+                lib_list = lib_list*"\"$(val)\","
+            end
+        end
+        push!(libs,lib=>lib_list)
+    end
+    # check if path already exists
+    if isfile(joinpath(path,file))
+        rm(joinpath(path,file))
+    end
+    # open a .md file and writes corresponding doc
+    open(joinpath(path,file),"w") do f
+        content = 
+        """
+        ```@meta
+        CollapsedDocStrings = true
+        ```
+
+        # Functions
+
+        ## Public API
+        ```@autodocs
+            Modules = [elastoPlasm]
+            Pages   = [$(libs["api"])]
+        ```
+        ## Internals
+        ```@autodocs
+            Modules = [elastoPlasm]
+            Pages   = [$(libs["program"])]
+        ```
+        """
+        write(f,content)
+    end
+    return file
+end
+
 format = Documenter.HTML()
 manual = [
     "Home" => "index.md",
     "Getting Started" => "getting_started.md",
-    "Functions" => mdGenerate(),
+    #"Functions" => mdGenerate(),
 ]
 @info "Making documentation..."
 makedocs(
@@ -16,6 +62,8 @@ makedocs(
     modules  = [elastoPlasm],
     format   = format,
     pages    = manual,
+    pagesonly= true,
+    checkdocs= :none,
 )
 @info "Deploying documentation..."
 deploydocs(
